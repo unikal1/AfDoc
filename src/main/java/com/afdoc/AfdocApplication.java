a0f56d0e7d269ae8616b88bdc26d92de22817fb7
unikal1 <jack8226123@gmail.com>
2025-02-22 17:07:09 +0900

    initial commit

---
+++ src/main/java/com/afdoc/AfdocApplication.java
@@ -0,0 +1,13 @@
+package com.afdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfdocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AfdocApplication.class, args);
+    }
+
+}
